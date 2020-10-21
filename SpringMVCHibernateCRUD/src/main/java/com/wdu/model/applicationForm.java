7ab70323c1f31d5ecb572ac6f9243c7fa70ab0d6
Prakash Koppal <prakashk827@gmail.com>
2020-10-21 19:03:47 +0530

    Spring and Hibernate configuration completed

---
+++ SpringMVCHibernateCRUD/src/main/java/com/wdu/model/applicationForm.java
@@ -0,0 +1,50 @@
+package com.wdu.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="applicationForm")
+public class applicationForm  {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String mobile;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	
+	
+
+}
---
+++ SpringMVCHibernateCRUD/src/main/webapp/WEB-INF/pages/dashBoard.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h1>This is dashBoard</h1>
+</body>
+</html>
